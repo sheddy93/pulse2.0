1160b300302ed313b1a535223e22269f9a28730c
sheddy93 <ihorshmid@gmail.com>
2026-04-30 23:03:06 +0200

    chore: remove frontend/components/company-card.js after move

--- frontend/components/company-card.js
+++
@@ -1,8 +0,0 @@
-export function CompanyCard({ company }) {
-  return (
-    <div className="detail-row">
-      <strong>{company.name}</strong>
-      <p>{company.plan || "Nessun piano"} - {company.users_count} utenti</p>
-    </div>
-  );
-}
