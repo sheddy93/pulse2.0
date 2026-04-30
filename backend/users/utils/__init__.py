3b951135734d08f456f1a7796ebc1935d5d8f593
sheddy93 <ihorshmid@gmail.com>
2026-04-30 22:52:42 +0200

    refactor: create utils/ package

---
+++ backend/users/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+PulseHR Utils Package
+"""
+from .email import *  # noqa
+from .geolocation import *  # noqa
+from .pricing import *  # noqa
+from .cookie_auth import *  # noqa
+from .automation_service import *  # noqa
