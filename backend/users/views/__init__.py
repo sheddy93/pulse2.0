72ba09ad18c22e3b8e7e1fdcf3ecc88b129e56bc
sheddy93 <ihorshmid@gmail.com>
2026-04-30 22:52:09 +0200

    refactor: create views/ package with __init__.py

---
+++ backend/users/views/__init__.py
@@ -0,0 +1,23 @@
+"""
+PulseHR Views Package
+Tutti i view modules sono importati qui per backward compatibility.
+"""
+from .auth import *  # noqa
+from .dashboard import *  # noqa
+from .leave import *  # noqa
+from .analytics import *  # noqa
+from .automation import *  # noqa
+from .companies import *  # noqa
+from .consultant import *  # noqa
+from .geolocation import *  # noqa
+from .payroll import *  # noqa
+from .safety import *  # noqa
+from .medical import *  # noqa
+from .reports import *  # noqa
+from .pricing import *  # noqa
+from .pricing_admin import *  # noqa
+from .push import *  # noqa
+from .saml import *  # noqa
+from .search import *  # noqa
+from .signature import *  # noqa
+from .stripe import *  # noqa
